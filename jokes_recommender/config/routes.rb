c40977e472ea4760507f530c400cb75f37c359a1
Marek Šabo <xsabo4@fi.muni.cz>
2016-11-16 23:35:18 +0100

    Design changes made
    - bootstrap added
    - routes fixed
    - useful gems inserted

--- jokes_recommender/config/routes.rb
+++ jokes_recommender/config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
+  root 'jokes#index'
+
 end
