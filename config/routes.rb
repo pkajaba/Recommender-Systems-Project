3dd27609a6b475c41009de3f81fb38cd93cd23c1
SilviaVig <silvia.vigasova@gmail.com>
2016-12-12 17:48:18 +0100

    [DEL] - removed testing features.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :jokes, only: :index
   resources :categories, only: :index
   resources :users, only: [:new, :create]
-  resources :ratings, only: [:update, :index]
+  resources :ratings, only: [:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
