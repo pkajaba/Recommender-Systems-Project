ef21f030dbe90b7fc58ef135b0759f96981957f7
Marek Šabo <xsabo4@fi.muni.cz>
2016-12-12 23:14:09 +0100

    Revert "ratings edit"
    This reverts commit 04dfdb0da7504afd61ce132a82b0a5237aa762d7.

--- app/logic/user_based_cf_strategy.rb
+++ app/logic/user_based_cf_strategy.rb
@@ -30,7 +30,8 @@ class UserBasedCFStrategy
     end
 
     #find @user ratings and other_user ratings for common jokes
-    other_user_ratings = find_ratings(otherUser, common_jokes).map { |rating| rating.user_rating }
+    our=find_ratings(otherUser, common_jokes)
+    other_user_ratings = our.map { |rating| rating.user_rating }
     user_ratings=find_ratings(@user, common_jokes).map { |rating| rating.user_rating }
 
     #should not happen but it happened :D
@@ -55,7 +56,7 @@ class UserBasedCFStrategy
     if divider == 0
       return [0, []]
     end
-    [numerator/divider, common_jokes]
+    [numerator/divider, our]
   end
 
   def find_ratings(user, jokes)
@@ -73,7 +74,7 @@ class UserBasedCFStrategy
     similarity_sums = Hash.new
     sims = (User.all - [@user]).map do |user|
       temp = pearson_correlation(user)
-      {sim: temp[0], user: user, common_jokes: temp[1]}
+      {sim: temp[0], user: user, common_ratings: temp[1]}
     end
 
     sims.sort! { |a, b| a[:sim] <=> b[:sim] }
@@ -81,7 +82,7 @@ class UserBasedCFStrategy
       if s[:sim] != 0
         user = s[:user]
         sim = s[:sim]
-        (user.jokes - s[:common_jokes]).each do |joke|
+        (user.jokes - s[:common_ratings]).each do |joke|
           totals[joke.id] ||= 0
           similarity_sums[joke.id] ||= 0
           totals[joke.id] += (find_rating(user,joke).user_rating - user.average) * sim
