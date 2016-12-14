d5beb6e7328c95e9edb9f93baad519e60d7f77eb
MartinMlynarik <martinmlynarik93@gmail.com>
2016-12-14 17:29:30 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,9 +75,9 @@ def create_filtered_data(jokes_per_category)
 end
 
 def store_jokes_to_db(index)
-  Rating.delete_all
-  Joke.delete_all
-  Category.delete_all
+  #Rating.delete_all
+  #Joke.delete_all
+  #Category.delete_all
 
   i = 0
   path = case index
@@ -108,8 +108,8 @@ def analyze_data
     pos = (jokes.length - i) / (jokes.length / 5)
     puts pos
     #length_categories[pos] += 1
-    categor = length_categories[pos]
-    categor.push(joke)
+    category = length_categories[pos]
+    category.push(joke)
     total_length += joke.content.length
   end
   average_length = total_length / jokes.length
@@ -123,22 +123,67 @@ def analyze_data
   end
 end
 
-def save_some_csv
-  CSV.open("data.csv", "wb") do |csv|
+def evaluate_rate(rating, min, max)
+  return 0 if rating - min < 0.1
+  max-min / rating-min
+end
+
+def normalize_rating(rating, average)
+  rating - average
+end
+
+def csv_jokes_length_in_categories
+  CSV.open('../graphs/csv_jokes_length_in_categories.csv', 'wb') do |csv|
     Joke.all.each do |joke|
       csv << [joke.content.length, joke.category.name]
     end
   end
 end
 
+def csv_category_popularity
+  #CSV.open('../graphs/csv_category_popularity.csv', 'wb') do |csv|
+    categories = Category.all.map { |category| [category.id, [0, 0] ] }
+    categories = Hash[categories.map {|key, value| [key, value]}]
+    categories
+    User.all.each do |user|
+      upcs = user.user_prefer_categories.select {|upc| upc.total_rated_jokes >= 2}
+      upcs.sort! {|a,b| a.average <=> b.average}
+      min = upcs.first
+      max = upcs.last
+      upcs.each do |upc|
+        categories[upc.category.id][0] += evaluate_rate(upc.average, min, max) #TODO este premysliet
+        categories[upc.category.id][1] += 1
+      end
+    end
+    categories.each do |key, value|
+      puts Category.find(key).name + ',' +(value[0]/value[1]]).to_s
+    end
+  #end
+end
+
+def csv_users_categories_normalized
+  #CSV.open('../graphs/csv_categories_normalized_rating.csv', 'wb') do |csv|
+    users = User.all.each.sort! {|user| user.ratings.length}
+    users.take(10).each do |user|
+      user.user_prefer_categories.select {|upc| upc.total_rated_jokes >= 2}
+    end
+
+    categories = Category.all.map { |category|}
+    Rating.all.each do |rating|
+      rating.user.average
+      rating.joke.category
+    end
+  #end
+end
 
 #MAIN
-
-# 0 -> from filtered_data else from all_jokes_selected_categories
-
 #create_filtered_data(20)
 #save_some_csv
 #analyze_data
-
 #store_jokes_to_db(0)
+
+
+csv_category_popularity
+
+
 
